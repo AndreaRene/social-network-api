6e56f00623ff876365eff3b757960334b3697a39
Andrea <andrea@interhact.net>
2022-10-31 17:57:51 -0500

    completed delete user and delete user thoughts in userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const { User } = require('../models');
+const { User, Thought } = require('../models');
 
 const userController = {
     // get all users
@@ -18,13 +18,13 @@ const userController = {
             )
             .catch((error) => res.status(500).json(error.msg));
     },
-    // create user
+    // create new user
     newUser(req, res) {
         User.create(req.body)
             .then((user) => res.json(user))
             .catch((error) => res.status(500).json(error.msg));
     },
-    // update user
+    // update user by id
     updateUser(req, res) {
         User.findOneAndUpdate(
             { _id: req.params.userId },
@@ -38,5 +38,15 @@ const userController = {
             )
             .catch((error) => res.status(500).json(error));
     },
-
+    // delete user by id
+    deleteUser(req, res) {
+        User.findByIdAndDelete({ _id: req.params.userId })
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'User ID does not exist' })
+                    : Thought.deleteMany({ _id: { $in: user.thoughts } })
+            )
+            .then(() => res.json({ message: 'User successfully deleted' }))
+            .catch((error) => res.status(500).json(error));
+    },
 }
