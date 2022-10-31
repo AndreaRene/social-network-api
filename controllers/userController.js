fff82b96d8b122459ad02bdb92f2fecf83001c3a
Andrea <andrea@interhact.net>
2022-10-31 17:50:29 -0500

    completed new user in userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,13 @@
 const { User } = require('../models');
 
 const userController = {
-    //get all users
+    // get all users
     getAllUsers(req, res) {
         User.find({})
             .then((users) => res.json(users))
             .catch((error) => res.status(500).json(error.msg));
     },
-    //get user by id
+    // get user by id
     getUserById(req, res) {
         User.findOne({ _id: req.params.userId })
             .select('-__v')
@@ -16,6 +16,12 @@ const userController = {
                     ? res.status(404).json({ message: 'User ID does not exist.' })
                     : res.json(user)
             )
+            .catch((error) => res.status(500).json(error.msg));
+    },
+    // create user
+    newUser(req, res) {
+        User.create(req.body)
+            .then((user) => res.json(user))
             .catch((error) => res.status(500).json(error.msg));
     },
 }
