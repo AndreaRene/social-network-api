a280387ebaedcc6bdff0f6347460f001887cdc2e
Andrea <andrea@interhact.net>
2022-10-31 21:25:59 -0500

    completed get thought by id in thoughtController

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -7,5 +7,15 @@ module.exports = {
             .then(thoughts => res.json(thoughts))
             .catch((error) => res.status(500).json(error.msg));
     },
-
+    // get thought by id
+    getThoughtById(req, res) {
+        Thought.findOne({ _id: req.params.thoughtId })
+            .select("-___v")
+            .then((thought) =>
+                !thought
+                    ? res.status(400).json({ message: 'Thought ID does not exist.' })
+                    : res.json(thought)
+            )
+            .catch((error) => res.status(500).json(error.msg));
+    },
 }
